d4a9a5123726349a0f45d439926158eb9c6decaf
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2023-02-08 20:31:35 +0800

    fix

--- index.tsx
+++ index.tsx
@@ -1,3 +1,12 @@
+import React from "react";
 export * as AntD from "antd";
 export * as Next from "@alifd/next";
 export * as BizCharts from "bizcharts";
+
+export async function foobar() {
+  await fetch("/foobar");
+}
+
+export function Foobar(props: any) {
+  return <div {...props}>Hi</div>;
+}
